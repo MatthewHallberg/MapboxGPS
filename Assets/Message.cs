87b1889b08b5025dd916b7cfd36befac6ae66c6d
matthew hallberg <matthewhallberg@gmail.com>
2017-11-27 01:54:24 -0500

    First commit, I think its working??? GPS positions faked for now, will allow user input next.

---
+++ Assets/Message.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message {
+
+	public double latitude;
+
+	public double longitude;
+
+	public string text;
+}
---
+++ Assets/SceneService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Mapbox.Examples;
+
+public class SceneService : MonoBehaviour {
+
+	private static SceneService instance;
+	public static SceneService Instance
+	{
+		get { return instance; }
+	}
+
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this){
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	public void SceneButtonDown(){
+		StartCoroutine (LoadSceneRoutine ());
+	}
+
+	IEnumerator LoadSceneRoutine(){
+		if (SceneManager.GetActiveScene ().name == "LocationProvider") {
+			//load AR Scene
+			AsyncOperation loadScene = SceneManager.LoadSceneAsync(1);
+			yield return loadScene;
+			MessageProvider.Instance.LoadARMessages ();
+		} else {
+			//load Mapbox scene
+			AsyncOperation loadScene = SceneManager.LoadSceneAsync(0);
+			yield return loadScene;
+			MessageProvider.Instance.LoadMapMessages ();
+		}
+	}
+}
